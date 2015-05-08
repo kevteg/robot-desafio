737da92d91d8b46a9d2705d1a94b7ee3734ada19
Kevin Hernandez <kevin.hernandez@unet.edu.ve>
2015-05-08 19:39:13 +0000

    Cambios en executable

--- Raspberry-pi/executable.sh
+++ Raspberry-pi/executable.sh
@@ -1,11 +1,12 @@
-    printf '\033[0;32m%s\033[0m\n'   " _   _      _               _       _  "
-    printf '\033[0;32m%s\033[0m\n'   "| | | |    (_)             | |     | |"
-    printf '\033[0;32m%s\033[0m\n'   "| |_| | ___ _ _ __ ___   __| | __ _| |"
-    printf '\033[0;32m%s\033[0m\n'   "|  _  |/ _ \ | '_ \` _ \ / _\` |/ _\` | |"
-    printf '\033[0;32m%s\033[0m\n'   "| | | |  __/ | | | | | | (_| | (_| | |"
-    printf '\033[0;32m%s\033[0m\n'   "\_| |_/\___|_|_| |_| |_|\__,_|\__,_|_|"
+#!/bin/sh
 
-    printf '\u001B[31m\n%s\n\n\n'   "Prepare to face your doom"
-    python2.7 robot-desafio/Raspberry-pi/engine.py
+printf '\033[0;32m%s\033[0m\n'   " _   _      _               _       _  "
+printf '\033[0;32m%s\033[0m\n'   "| | | |    (_)             | |     | |"
+printf '\033[0;32m%s\033[0m\n'   "| |_| | ___ _ _ __ ___   __| | __ _| |"
+printf '\033[0;32m%s\033[0m\n'   "|  _  |/ _ \ | '_ \` _ \ / _\` |/ _\` | |"
+printf '\033[0;32m%s\033[0m\n'   "| | | |  __/ | | | | | | (_| | (_| | |"
+printf '\033[0;32m%s\033[0m\n'   "\_| |_/\___|_|_| |_| |_|\__,_|\__,_|_|"
 
-
+printf '\033[0;31m%s\n\n\n'   "Prepare to face your doom"
+cd ~/robot-desafio/Raspberry-pi
+python2.7 engine.py
