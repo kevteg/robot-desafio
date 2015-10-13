57226abd6ff8f2194f97b852f2aae73c87f9928b
Kevin Hernández <kevin.hernandez@unet.edu.ve>
2015-10-13 18:03:23 -0430

    Se agrega número de pin de led encendido

--- Arduino/robot-des-ino/robot-des/robot-des.ino
+++ Arduino/robot-des-ino/robot-des/robot-des.ino
@@ -1,4 +1,3 @@
-
 /*Motor*/
 #include <AFMotor.h>
 #include <motor.h>
@@ -22,30 +21,30 @@
 
 //Motores:
 //Avance
-#define _steps_per_round 200
-#define _pin_dir_motor_avance 12
-#define _pin_step_motor_avance 13
-#define _velocidad_motor_avance 4
+#define _steps_per_round          200
+#define _pin_dir_motor_avance     12
+#define _pin_step_motor_avance    13
+#define _velocidad_motor_avance   4
 //Limpieza
-#define _pin_motor_limpieza 2
+#define _pin_motor_limpieza       2
 #define _velocidad_motor_limpieza 255
 //Sensor ultrasonido
-#define _trigger_pin 14
-#define _echo_pin 19
-#define _max_distan_us 5 //Distancia de maleza al suelo
+#define _trigger_pin              14
+#define _echo_pin                 19
+#define _max_distan_us            5 //Distancia de maleza al suelo
 //Leds
-#define _led_maleza 5
-#define _led_punto_caliente 6
+#define _led_maleza               5
+#define _led_punto_caliente       6
+#define _led_iluminacion          7
 //Pantalla
-#define _screen_adr 0x27
-#define _screen_colms 16
-#define _screen_rows 2
-
+#define _screen_adr               0x27
+#define _screen_colms             16
+#define _screen_rows              2
 
 using namespace robot;
 engine Robot(_pin_motor_limpieza, _pin_dir_motor_avance, _steps_per_round, _pin_step_motor_avance, _velocidad_motor_avance, _velocidad_motor_limpieza, 
             _trigger_pin, _echo_pin, _max_distan_us, 
-            _led_maleza, _led_punto_caliente,
+            _led_maleza, _led_punto_caliente, _led_iluminacion,
             _screen_adr, _screen_colms, _screen_rows);
 
 void setup() {
